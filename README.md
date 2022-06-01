2a017c0f88192993d776a1b29733ddea13a570fc
Ali Kürşat Yıldırım <aynurkursat1999@hotmail.com>
2022-06-01 20:43:28 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Movie-Website
+# Using React with Go
+
+Clone this repository, open a terminal window, cd into the project directory, and execute:
+
+~~~
+npm install
+~~~
+
+To run the app in development mode, execute:
+
+~~~
+npm start
+~~~
---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component, Fragment } from 'react';
+import {BrowserRouter as Router, Switch, Route, Link, useParams, useRouteMatch} from 'react-router-dom';
+import Movies from './components/Movies';
+import Admin from './components/Admin';
+import Home from './components/Home';
+import Categories from './components/Categories';
+import OneMovie from './components/OneMovie';
+
+export default function App() {
+  return (
+    <Router>
+    <div className="container">
+
+      <div className="row">
+        <h1 className="mt-3">
+          Go Watch a Movie!
+        </h1>
+        <hr className="mb-3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <ul className="list-group">
+              <li className="list-group-item">
+                <Link to="/">Home</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/movies">Movies</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/by-category">Categories</Link>
+              </li>
+              <li className="list-group-item">
+                <Link to="/admin">Manage Catalogue</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div className="col-md-10">
+          <Switch>
+            
+            <Route path="/movies/:id" component={OneMovie} />
+
+            <Route path="/movies">
+              <Movies />
+            </Route>
+            <Route exact path="/by-category">
+              <CategoryPage />
+            </Route>
+
+            <Route 
+            exact
+            path="/by-category/drama"
+            render={(props) => <Categories {...props} title={`Drama`} />}
+            />
+
+          <Route 
+            exact
+            path="/by-category/comedy"
+            render={(props) => <Categories {...props} title={`Comedy`} />}
+            />
+
+            <Route path="/admin">
+              <Admin />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </div>
+    </Router>
+  );
+}
+
+function CategoryPage() {
+
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Categories</h2>
+
+      <ul>
+        <li><Link to={`${path}/comedy`}>Comedy</Link> </li>
+        <li><Link to={`${url}/drama`}>Drama</Link> </li>
+      </ul>
+    </div>
+  );
+}

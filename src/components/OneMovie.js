a9ae93efcf959281e6ff56f234dedbf835592855
AIKursat <aynurkursat1999@hotmail.com>
2022-06-11 16:08:26 +0300

    Merge branch 'main' of https://github.com/AIkursat/Movie-Website

--- src/components/OneMovie.js
+++ src/components/OneMovie.js
@@ -3,57 +3,30 @@ import React, { Component, Fragment } from "react";
 export default class OneMovie extends Component {
   state = { movie: {}, isLoaded: false, error: null };
 
-    state = { movie: {}, ilLoaded : false, error:null };
-
-    componentDidMount() {
-        fetch("http://localhost:4000/v1/movie" + this.props.match.params.id) // It will get one movie
-            // .then((response) => response.json())
-            .then((response) => {
-                console.log("status code is", response.status);
-                if (response.status !== "200") {
-                    let err = Error;
-                    err.message = "invalid response code " + response.status
-                    this.setState({ error: err });
-                }
-                return response.json();
-            })
-            .then((json) => {
-                this.setState({
-                    movie: json.movie,
-                    isLoaded: true,
-                },
-                    (error) => {
-                        this.setState({
-                            isLoaded: true,
-                            error
-                        });
-                    }
-                );
+  componentDidMount() {
+    fetch("http://localhost:4000/v1/movie/" + this.props.match.params.id)
+      .then((response) => {
+        if (response.status !== "200") {
+          let err = Error;
+          err.message = "Invalid response code: " + response.status;
+          this.setState({ error: err });
+        }
+        return response.json();
+      })
+      .then((json) => {
+        this.setState(
+          {
+            movie: json.movie,
+            isLoaded: true,
+          },
+          (error) => {
+            this.setState({
+              isLoaded: true,
+              error,
             });
-    }
-
-    render() {
-        
-        return (
-            <Fragment>
-                <h2>Movie: {this.state.movie.title} {this.state.movie.id}</h2>
-
-                <table className="table table-compact table-striped">
-                    <thead></thead>
-                    <tbody>
-                        <tr>
-                            <td><strong>Title:</strong></td>
-                            <td>{this.state.movie.title}</td>
-                        </tr>
-                        <tr>
-                            <td><strong>Run time:</strong></td>
-                            <td>{this.state.movie.runtime} minutes</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </Fragment >
+          }
         );
-      };
+      });
   }
 
   render() {
@@ -112,6 +85,6 @@ export default class OneMovie extends Component {
           </table>
         </Fragment>
       );
-    };
-  };
-
+    }
+  }
+}

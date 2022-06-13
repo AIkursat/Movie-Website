f84c69d110742c02b05a7a7bf3d43167b55bae63
AIKursat <aynurkursat1999@hotmail.com>
2022-06-13 22:06:46 +0300

    Delete movie

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -4,6 +4,7 @@ import Input from "./form-components/Input";
 import TextArea from "./form-components/TextArea";
 import Select from "./form-components/Select";
 import Alert from "./ui-components/Alert";
+import { Link } from "react-router-dom";
 
 export default class EditMovie extends Component {
   constructor(props) {
@@ -133,6 +134,10 @@ export default class EditMovie extends Component {
     }
   }
 
+  confirmDelete = (e) => {
+    console.log("would delete movie id", this.state.movie.id);
+  }
+
   render() {
     let { movie, isLoaded, error } = this.state;
 
@@ -213,6 +218,15 @@ export default class EditMovie extends Component {
             <hr />
 
             <button className="btn btn-primary">Save</button>
+            <Link to="/admin" className="btn btn-warning ms-1">
+              Cancel
+            </Link>
+            {movie.id > 0 && (
+              <a href="#!" onClick={() => this.confirmDelete()}
+              className="btn btn-danger ms-1">
+                Delete
+              </a>
+            )}
           </form>
         </Fragment>
       );
